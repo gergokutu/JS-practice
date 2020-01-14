7d28492c014edf4b4272b7728936ce34666a7c5b
gergokutu <gergo.kutu@gmail.com>
2020-01-14 17:01:56 +0100

    Comments

--- 003_global_vs_local.js
+++ 003_global_vs_local.js
@@ -12,3 +12,16 @@
 })()
 
 // the engine takes our code and executes it
+// ECMAScript (ES) is a standard
+// Javascript is a implementation of the standard (ES)
+// browser is the runtime environment
+// JS engine is part of the browser
+// node.js > environment for back-end JS codes
+// JS > JIT compilation (JS engine compiles executable...)
+// truth > up to the engine if JS is compiled or interpreted
+// it's an implementation detail..
+
+// var, let, const - global vs local
+// var > function scope (goes up if does not find)
+// let > block scope
+// const > block, values cannot be reassignable
