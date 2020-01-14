df6b205a02d2de880d64e19dc283964d369480c4
gergokutu <gergo.kutu@gmail.com>
2020-01-14 17:19:54 +0100

    Block scope

--- 003_global_vs_local.js
+++ 003_global_vs_local.js
@@ -25,3 +25,34 @@
 // var > function scope (goes up if does not find)
 // let > block scope
 // const > block, values cannot be reassignable
+
+// attached to the window object
+// check in the console > window.y > 10
+// this is polluting the global variable...
+// it can be overwritten accidentally...
+var y = 10
+
+// similar to var
+// not stored on window
+// but in a declarative environment
+// check in the console > window.z > undefined
+// accessible globally
+// block scope {}
+let z = 20
+{
+  let y = 1000
+  let z2 = 25
+  console.log(y) // 1000
+  console.log(z) // 20
+  console.log(z2) // 25
+}
+console.log(y) // 10
+console.log(z) // 20
+// console.log(z2) // error > is not defined
+
+console.log(y, z) // 10 20
+
+// cannot change the value
+const x = 30
+
+// how to replace IIFE > e.g. block scope (let)
