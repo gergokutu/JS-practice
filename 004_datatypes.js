045e81eaa6eec193a1461576b94ff33c074edc30
gergokutu <gergo.kutu@gmail.com>
2020-01-14 17:51:11 +0100

    Primitives have their counterparts

--- 004_datatypes.js
+++ 004_datatypes.js
@@ -26,3 +26,21 @@ console.log(grades[0])
 
 let index = 3
 console.log(grades[index])
+
+{
+  // primitives are immutable
+  let myName = "Pocok"
+  myName.toLocaleUpperCase()
+  console.log(myName) // Pocok
+  myName = myName.toLocaleUpperCase()
+  console.log(myName) // POCOK
+}
+
+{
+  // primitives has their object counterpart
+  // object counterparts can have properties!!
+  let myName = "Pocok"
+  let yourName = new String("Mogyi")
+  console.log(typeof(myName)) // string
+  console.log(typeof(yourName)) // object
+}
