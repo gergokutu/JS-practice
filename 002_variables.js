eb4977c2d84c9191a39c09a6cea0f0fb2c6a380d
gergokutu <gergo.kutu@gmail.com>
2020-01-14 16:09:41 +0100

    Var, prompt, alert, console.log

---
+++ 002_variables.js
@@ -0,0 +1,11 @@
+// dynamic > app content is changing
+// ask user input
+// does not persist, clear out with every page load
+var userName = prompt("What is your name?")
+alert(`Hello ${userName}!`)
+console.log("userName:", userName)
+
+/*
+  multiline
+  comment
+*/
