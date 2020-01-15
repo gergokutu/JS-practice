60ae51b360ab81cc9bf322d490b3bc803bdf28ef
gergokutu <gergo.kutu@gmail.com>
2020-01-15 10:31:53 +0100

    Arithmetic operators

---
+++ 005_operators.js
@@ -0,0 +1,23 @@
+// arithmetic operators
+{
+  var slicesOfPizza = 10;
+  console.log(slicesOfPizza + 5)
+  console.log(slicesOfPizza - 5)
+  console.log(slicesOfPizza * 5)
+  console.log(slicesOfPizza / 5)
+  console.log(slicesOfPizza % 5)
+
+  var presedence1 = 5 + 3 * 12 - 20 / 10
+  var presedence2 = 5 + (3 * 12) - (20 / 10)
+  var presedence3 = (5 + 3) * 12 - (20 / 10)
+  console.log(presedence1, presedence2, presedence3)
+
+  // MDN > operaton presedence chart
+  // modulo (%) same level as * or /
+  // association > left-to-right (default), right-to-left
+}
+
+// other operators
+{
+  
+}
