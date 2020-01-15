d4dbf34fd3f8e73f2eafeaa4f1dd3d7b2af47f37
gergokutu <gergo.kutu@gmail.com>
2020-01-15 15:11:21 +0100

    More string methods

--- 006_strings.js
+++ 006_strings.js
@@ -51,3 +51,45 @@ And the new line is totally ok!!! ;)`)
   console.log(favFood.lastIndexOf("t")) // 6
 
 }
+
+// more string methods
+{
+  // subtstring method > take out a part of the string
+  // index to start at (inclusive), index to stop at (exclusive)
+  let content = "Today we'll be talking about string methods and how fun they are"
+  console.log(content.substring(6, 8)) // we
+
+  // substr
+  // index to start at (inclusive), nuber of chars we want to include
+  console.log(content.substr(6, 2)) // we
+
+  // really similar to substring
+  // diff > when working with negative numbers...
+  console.log(content.slice(6, 8)) // we
+
+  // how to modify a string
+  // always return a new string
+  // keep it > save to a new variable
+  let upper = content.toUpperCase()
+  let lower = content.toLowerCase()
+  console.log(upper)
+  console.log(lower)
+  console.log(content)
+
+  // trim any kind of white space before and after
+  // useful for form validation
+  let content2 = " \tToday we'll be talking about string methods and how fun they are\n\t"
+  console.log(content2.trim())
+  console.log(content2.trimLeft())
+  console.log(content2.trimRight())
+  // exactly the same
+  console.log(content2.trimStart())
+  console.log(content2.trimEnd())
+
+  // repeat
+  console.log("tick-tack ".repeat(3))
+
+  // split by delimiter
+  // returns an array
+  console.log(content.split(" "))
+}
