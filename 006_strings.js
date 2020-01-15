3c57f40f6d915e064e8b0d6aab9b9e584cf96d00
gergokutu <gergo.kutu@gmail.com>
2020-01-15 14:43:22 +0100

    String methods

--- 006_strings.js
+++ 006_strings.js
@@ -19,3 +19,35 @@
   hmmmm.....
 And the new line is totally ok!!! ;)`)
 }
+
+// string methods
+{
+  // String.
+  // myName.
+  let favFood = "spagetti"
+  console.log(favFood.charAt(0))
+  // favFood is not gonna be modified
+  // if you want to store it
+  // do it in a new variable
+  console.log(favFood.concat(" is supercool", "! ", "But", " ", "pizza as well!"))
+
+  // Finding a string in anouther string
+  let search1 = "age"
+  let search2 = "word"
+  console.log(favFood.includes(search1)) // true
+  console.log(favFood.includes(search2)) // false
+  // 2nd argument > serachIn.includes(search_what, index_to_start_at)
+  console.log(favFood.includes(search1, 3)) // false
+
+  // where things are
+  console.log(favFood.indexOf(search1)) // 2
+  console.log(favFood.indexOf(search1, 2)) // 2
+  // find the second "t"
+  console.log(favFood.indexOf("t", favFood.indexOf("t") + 1)) // 6
+  console.log(favFood.includes(search2)) // -1 (false)
+
+  // last index
+  // works like the prev
+  console.log(favFood.lastIndexOf("t")) // 6
+
+}
