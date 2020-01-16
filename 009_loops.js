382d3ff99858645b33901f304168ac0aae5a7f06
gergokutu <gergo.kutu@gmail.com>
2020-01-16 13:48:47 +0100

    While, do-while, for

--- 009_loops.js
+++ 009_loops.js
@@ -15,3 +15,25 @@ for (let i = 0; i < 10; i++) {
   console.log(i)
 }
 */
+
+{
+  // while
+  // try with i = 10
+  let i = 0
+  while (i < 10) {
+    console.log("while loop:", i)
+    i++
+  }
+  // do while > if you want the loop running at least 1 time
+  // and check the condition after that
+  let j = 10
+  do {
+    console.log("do-while loop:", j)
+    j++
+  } while (j < 10)
+
+  // for
+  for (let i = 0; i < 10; i++) {
+    console.log("for loop:", i)
+  }
+}
