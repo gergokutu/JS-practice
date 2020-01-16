06d1bb7bb1006b3003b6d02612864fa79cb0625f
gergokutu <gergo.kutu@gmail.com>
2020-01-16 14:21:22 +0100

    Search a char in a string

--- 009_loops.js
+++ 009_loops.js
@@ -34,7 +34,8 @@ for (let i = 0; i < 10; i++) {
   do {
     console.log("do-while loop:", j)
     j++
-  } while (j < 10)
+  }
+  while (j < 10)
 
   // for
   // condition first
@@ -44,9 +45,36 @@ for (let i = 0; i < 10; i++) {
 }
 
 {
-  let password
+  // let password
 
-  do {
-    password = prompt("What's the password")
-  } while (password.toLowerCase !== "secret")
+  // do {
+  //   password = prompt("What's the password")
+  // }
+  // while (password.toLowerCase() !== "secret")
+}
+
+{
+  // count even numbers
+  for (let i = 1; i < 10; i = i + 2) {
+    console.log("even:", i)
+  }
+
+  // count down with odd numbers
+  // 0 included
+  for (let i = 10; i >= 0; i -= 2) {
+    i != 0 ? console.log(i) : console.log("BAAAMMMM")
+  }
+
+  let list = ["Yippie", "Yuppieeee", 5, 6, { name: "Pocok", title: "code"}]
+  for (let i = 0; i < list.length; i++) {
+    console.log(list[i])
+  }
+
+  // search for a particular char in a string
+  let name = "Pocok"
+  for (let i = 0; i < name.length; i++) {
+    name[i] === 'c' ? 
+      console.log(`Found "c" letter on place ${i + 1} of the word "Pocok".`) :
+      console.log(name[i])
+  }
 }
