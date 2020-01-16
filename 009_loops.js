6da1bc83260f36178570b45a9611f5809d504cbb
gergokutu <gergo.kutu@gmail.com>
2020-01-16 13:36:19 +0100

    Intro to loops

---
+++ 009_loops.js
@@ -0,0 +1,17 @@
+/*
+Loops
+- ICU: Initialization, Condition, Update
+- while
+
+let i = 0
+while (i < 10) {
+  console.log(i)
+  i++
+}
+
+- for (usually with certain number of loops)
+
+for (let i = 0; i < 10; i++) {
+  console.log(i)
+}
+*/
