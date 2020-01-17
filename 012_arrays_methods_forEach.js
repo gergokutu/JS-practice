c36c4ab2c7bdcdf61f64c50ba3c06541dcb5fd81
gergokutu <gergo.kutu@gmail.com>
2020-01-17 13:36:24 +0100

    Array methods - for, forEach, map

---
+++ 012_arrays_methods_forEach.js
@@ -0,0 +1,25 @@
+{
+  // iterating through an array with forEach method
+  let grades = [12, 22, 33, 44, 55]
+  grades.length = 7
+
+  // standard way to itarate through the array
+  // but it takes the undefined(s)
+  for (let i = 0; i < grades.length; i++) {
+    // correct undefined
+    if (grades[i] !== undefined) console.log("for:", grades[i])
+  }
+
+  // forEach ignores them
+  // pass a callback to forEach
+  grades.forEach(function(item){
+    console.log("forEach:", item)
+  })
+
+  // with arrow functon
+  grades.forEach(item => console.log("forEach => :", item))
+
+  // with map
+  // ignores the undefined as well
+  grades.map(item => console.log("map:", item))
+}
