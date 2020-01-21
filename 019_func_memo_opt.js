f1234a928bab6b70bb5109e91126f9cccb5bb3ac
gergokutu <gergo.kutu@gmail.com>
2020-01-21 16:49:18 +0100

    Memoization by using an object as pow.calculated

--- 019_func_memo_opt.js
+++ 019_func_memo_opt.js
@@ -6,25 +6,46 @@
 // and do not want to recalculate the same values
 // e.g calculate the factorial/pow for large numbers
 {
-  pow.calculated = []
-  console.log(pow.calculated)
+  // pow.calculated = []
+  // look up if the value is already calculated
+  // we need a key value pair system
+  // we have to track the input besides the output value
+  // use objet instead of array
+  pow.calculated = {}
 
   function pow(base, toPow) {
-    let total = 1
+    // next string gonna be the key
+    let stringVersion = base + "^" + toPow
+    console.log("String:", stringVersion)
 
+    // check the value
+    // The in operator returns true
+    // if the specified property is in the specified object
+    // or its prototype chain.
+    // check MDN!!!
+    if (stringVersion in pow.calculated) {
+      console.log("Found")
+      return pow.calculated[stringVersion]
+    }
+
+    console.log("Calculated!")
+    let total = 1
     for (let i = 0; i < toPow; i++) {
       total *= base
     }
 
-    pow.calculated.push(total)
+    // pow.calculated.push(total)
+    // now it's a object
+    // key > stringVersion, value > total
+    pow.calculated[stringVersion] = total
     console.log(pow.calculated)
     return total
   }
 
   pow(3, 3)
   pow(3, 3)
-
-
+  pow(3, 4)
+  pow(5, 3)
 
   // function factorial(num) {
   //   let result = 1
