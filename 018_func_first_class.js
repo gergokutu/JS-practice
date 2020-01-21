7dc1f98c9d0e70d1726e5ee101358af96f179873
gergokutu <gergo.kutu@gmail.com>
2020-01-21 15:50:15 +0100

    Invoke the returned pow()

--- 018_func_first_class.js
+++ 018_func_first_class.js
@@ -27,4 +27,20 @@
   // describe the function in a property
   pow.description = "Raise the passed base to the passed power. Returns a number."
   console.log(pow.description)
+
+  // pass as an argument
+  function callbackExample(callback) {
+    return callback(2, 4)
+  }
+  console.log(callbackExample(pow))
+
+  // return a function
+  function returnFunction(func) {
+    return pow
+  }
+  
+  // call the returned function with base = 2, pow = 5
+  const base = 2
+  const toPow = 5
+  console.log(returnFunction()(base, toPow))
 }
