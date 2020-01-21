2fb560f60a53948bcba13440f4eca006c7912522
gergokutu <gergo.kutu@gmail.com>
2020-01-21 17:36:12 +0100

    Rest param

--- 020_func_param.js
+++ 020_func_param.js
@@ -1,12 +1,15 @@
 // Default Parameters, Rest Parameters, Implicit Parameters
 {
+  // dealing with less params > default params
   // default y
-  function pow(x, y) {
+  // more params > ...extra (spread synthax)
+  function pow(x, y = 2, ...extra) {
+    console.log('Extra:', extra)
     // instead of default y
     // if (y === undefined) { y = 2 }
     // with ternary
     // y = typeof y === "undefined" ? 2 : y
-    
+
     let total = 1
 
     for (let i = 0; i < y; i++) {
@@ -17,4 +20,20 @@
   }
 
   console.log(pow(3))
+  // passing more params
+  // after defined ones > arguments ignored by the function
+  console.log(pow(3, 3, 3, 3))
+
+  // when unlimited arguments are useful
+  function largest(x, ...extra) {
+    let largest = x
+
+    for (let i = 0; i < extra.length; i++) {
+      largest < extra[i] ? largest = extra[i] : largest
+    }
+
+    return largest
+  }
+
+  console.log(largest(1, 4, 55, 6, 7, 89, 3))
 }
