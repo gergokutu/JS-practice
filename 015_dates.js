5eb5a3bea4c2d85f5b8698c222e539c5e7fdf0d8
gergokutu <gergo.kutu@gmail.com>
2020-01-21 10:43:12 +0100

    Date methodes

--- 015_dates.js
+++ 015_dates.js
@@ -67,3 +67,36 @@ Date.now()
   let oneDay = 1000 * 60 * 60 * 24
   console.log("Days:", days / oneDay)
 }
+
+// Date methods
+{
+  // not worls in all browsers
+  // millisecs
+  let myDate = Date.parse('12 Jan 1995 00:15:54 GMT')
+  // same in date format
+  let myDate2 = new Date('12 Jan 1995 00:15:54 GMT')
+  let myDate3 = new Date('1995-01-12')
+  console.log("Millisecs:", myDate)
+  console.log(myDate2)
+  console.log(myDate3)
+
+  // there are also a lot of JS libraries with dates
+
+  let myDate4 = new Date(2012, 11, 15, 10, 10, 10)
+  console.log(myDate4)
+  let myDate5 = new Date(Date.UTC(2012, 11, 15, 10, 10, 10))
+  console.log("UTC conversion:", myDate5)
+  console.log(myDate5.getFullYear())
+  console.log(myDate5.getMonth())
+
+  // check the documentation...
+
+  // setters > set the dates
+  // e.g the hours to 12
+  myDate5.setHours(12)
+  console.log(myDate5)
+
+  // how many minutes my timezone is offset from GMT (UTC)
+  console.log("Offset in minutes", myDate5.getTimezoneOffset())
+  console.log("Offset in hours", myDate5.getTimezoneOffset() / 60)
+}
