bbfeb90c601ac5700f432218e182a089bcd59d1c
gergokutu <gergo.kutu@gmail.com>
2020-01-22 13:31:12 +0100

    Arrow func intro

---
+++ 022_arrow.js
@@ -0,0 +1,14 @@
+/*
+Arrow functions
+- new way to create the function
+- (a, b) => expression
+- () => {
+  return...
+}
+- let d = x => x + x
+- etc.
+- there are couple of variotons for the synthax (one line, with args...)
+- the value of "this" always gonna be the same
+  - value determined by => where the function is created
+  - and not how it's called...
+*/
