a2059739fc788bac3e309f3e899550ed6633df9d
gergokutu <gergo.kutu@gmail.com>
2020-01-23 11:59:54 +0100

    Polymorphism example

--- 027_OOP_3.js
+++ 027_OOP_3.js
@@ -8,3 +8,34 @@ Polymorphism
 - or as its parent, or parent's parent...
 - we can treat both student and teacher as user
 */
+{
+  let user = {
+    active: true,
+    sayHello: function() {
+      return `${this.name} says hi!`
+    }
+  }
+
+  let student = {
+    name: "Pupil",
+    major: "English"
+  }
+
+  let teacher = {
+    name: "Master",
+    teaching: ["math", "science"],
+    sayHello: function() {
+      let message = this.name + " teaches "
+      this.teaching.forEach(e => message += e + " ")
+      return message
+    }
+  }
+
+  Object.setPrototypeOf(teacher, user)
+  Object.setPrototypeOf(student, user)
+
+  let newMembers = [teacher, student]
+  
+  // handle users and teachers as they would be the same type
+  newMembers.forEach(element => console.log(element.sayHello()))
+}
