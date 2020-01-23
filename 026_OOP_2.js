9f601d4a58e3e150c5085ab91ebe53a8d2ba5e02
gergokutu <gergo.kutu@gmail.com>
2020-01-23 10:28:17 +0100

    Prototype inheritance notes

---
+++ 026_OOP_2.js
@@ -0,0 +1,16 @@
+/*
+Prototype inheritance
+- allows an object to take parts from another one
+- inherit thet parts as its own
+- in JS it'S done by the prototype
+- prototype is an object where the object inherits from
+- saving memory > put methods on prototype's constructor
+- prevents the multiplication of that method on every object
+- the object always inherits the prototype (__proto__)
+- prototype also might have a prototype (prototype chain)
+- root prototype > null
+- Object.prototype
+
+Prototypes and Constructors
+- 
+*/
