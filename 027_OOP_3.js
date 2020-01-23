dd19836584242cc96262600c19800c667f4af289
gergokutu <gergo.kutu@gmail.com>
2020-01-23 11:38:16 +0100

    Polymorphism intro

---
+++ 027_OOP_3.js
@@ -0,0 +1,10 @@
+/*
+Polymorphism
+- how to handle an object
+- as itself or a part of the inheritance hierarchy
+- e.g. user vs student
+- other words >
+- an object is handled as the type of itself
+- or as its parent, or parent's parent...
+- we can treat both student and teacher as user
+*/
