7ad2cc8a5f624255519b2de2dbe9aab66905bf1b
gergokutu <gergo.kutu@gmail.com>
2020-01-23 09:16:39 +0100

    Intro to Object Oriented JS

---
+++ 025_OOP.js
@@ -0,0 +1,11 @@
+/*
+Object Oriented Javascript
+- a bit different from OOP with other languages
+- an object describes an entity, it can model the real world
+- constructor function same as classes in JS
+- but diff syntax
+- this in constructor function (ctor)
+- ctor does not return anything >
+- use new keyword to create a new instance
+- and assign it to a new variable
+*/
