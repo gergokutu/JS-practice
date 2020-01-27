f33406bd564b0137b632215a6203bf2cb0512167
gergokutu <gergo.kutu@gmail.com>
2020-01-27 13:38:30 +0100

    Try hasOwnProperty() method

--- 029_OOP_5.js
+++ 029_OOP_5.js
@@ -30,10 +30,18 @@
   newMembers.forEach(element => console.log(element.sayHello()))
 
   // check programatically if the object has a name property
+  // also checks the prototype > active
   console.log("Name in teacher:", "name" in teacher) // true
 
   // similar way with doing a test
-  console.log("Name in teacher:", teacher.name !== undefined)
+  // also checks the prototype > active
+  console.log("Name in teacher 2:", teacher.name !== undefined)
 
   console.log(teacher)
+
+  // hasOwnProperty() method
+  // checks directly the object and not the proptotype
+  // "active" property on user > false
+  // "name" property on teacher > true
+  console.log("hasOwnProperty:", teacher.hasOwnProperty("name"))
 }
